6ecd2b6d96953eb52a2a8391a520b53d70637b09
BadFrank <38784895+FrancescoVinci@users.noreply.github.com>
2024-07-22 13:21:17 +0200

    Add evaluations

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from tsp import apply_tsp_to_medoids, assign_doc_ids, reassign_postings, sort_po
 from rich.console import Console
 from rich.table import Text
 from log import print_table, print_ii_info, print_cluster_info
+import time
+import matplotlib.pyplot as plt
 
 
 def parse_dat_file(file_path):
@@ -12,7 +14,7 @@ def parse_dat_file(file_path):
     current_doc_id = 0
     with open(file_path, 'r') as file:
         for line in file:
-            if current_doc_id >= 36000: # block to 36000 for time reason
+            if current_doc_id >= 27000: # block to 36000 for time reason
                 break
             if line.startswith('.I'):
                 current_doc_id = int(line.split()[1])
@@ -46,30 +48,70 @@ def main(file_path, block_size):
 
     print_table(vb_coding, gamma_coding, delta_coding)
 
-    console.print(Text("Clustering...\n", style="blue"))
-
-    radius = 0.95
-    clusters, forward_index = clustering(inverted_index, radius)
-
-    print_cluster_info(radius, len(clusters))
-
-    console.print(Text("Solving TSP.....\n", style="blue"))
-
-    tsp = apply_tsp_to_medoids(forward_index, clusters)
-    map_docID = assign_doc_ids(tsp, clusters)
-
-    console.print(Text("Reassigning docIDs...\n", style="blue"))
     
-    new_inverted_index = reassign_postings(inverted_index, map_docID)
+    radiuses = [0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
+    times = []
+    num_clusters = []
+    avg_bytes_vb = []
+    avg_bytes_gamma = []
+    avg_bytes_delta = []
 
-    d_gaps_new_index = convert_to_d_gaps(sort_postings(new_inverted_index))
+    for radius in radiuses:
+        print("---------------------------")
 
-    # delta, gamma, VB for the average of bytes used to store the d-gaps
-    vb_coding_new = variable_byte_codes(d_gaps_new_index)
-    gamma_coding_new = elias_gamma_code(d_gaps_new_index)
-    delta_coding_new = elias_delta_code(d_gaps_new_index)
+        console.print(Text(f"Clustering... (radius= {radius})\n", style="bold blue"))
 
-    print_table(vb_coding_new, gamma_coding_new, delta_coding_new, True)
+        start_time = time.time()
+        clusters, forward_index = clustering(inverted_index, radius)
+
+        print_cluster_info(radius, len(clusters))
+
+        console.print(Text("Solving TSP.....\n", style="blue"))
+
+        tsp = apply_tsp_to_medoids(forward_index, clusters)
+        map_docID = assign_doc_ids(tsp, clusters)
+
+        console.print(Text("Reassigning docIDs...\n", style="blue"))
+    
+        new_inverted_index = reassign_postings(inverted_index, map_docID)
+        end_time = time.time()
+        tot_time = end_time - start_time
+
+        d_gaps_new_index = convert_to_d_gaps(sort_postings(new_inverted_index))
+
+        # delta, gamma, VB for the average of bytes used to store the d-gaps
+        vb_coding_new = variable_byte_codes(d_gaps_new_index)
+        gamma_coding_new = elias_gamma_code(d_gaps_new_index)
+        delta_coding_new = elias_delta_code(d_gaps_new_index)
+
+        print_table(vb_coding_new, gamma_coding_new, delta_coding_new, True)
+        times.append(tot_time)
+        num_clusters.append(len(clusters))
+        avg_bytes_vb.append(vb_coding_new)
+        avg_bytes_gamma.append(gamma_coding_new)
+        avg_bytes_delta.append(delta_coding_new)
+    
+    plt.figure(0)
+    plt.plot(num_clusters, times, marker='o')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Execution Time (seconds)')
+    plt.title('Execution Time vs. Number of Clusters')
+    plt.grid(True)
+    plt.savefig("./plot/execution_time_clusters.png")
+
+    plt.figure(1)
+    plt.plot(num_clusters, avg_bytes_vb, label="Avg. Bytes VB" ,marker='o')
+    plt.plot(num_clusters, avg_bytes_gamma, label="Avg. Bytes Gamma" ,marker='o')
+    plt.plot(num_clusters, avg_bytes_delta, label="Avg. Bytes Delta" ,marker='o')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Bytes')
+    plt.title('Compression rate per number of clusters')
+    plt.grid(True)
+    plt.legend() 
+    plt.savefig("./plot/compression_rate_clusters.png")
+
+
+
 
 
 
